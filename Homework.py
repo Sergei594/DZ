71313ed19d1f2e72d92fdc548477fd4decb9cc50
Sergei <serfe121y@rambler.ru>
2023-07-19 14:34:20 +0300

    34,36

--- Homework.py
+++ Homework.py
@@ -235,20 +235,20 @@
 # print(m_set2)
 # s_set = sorted(n_set1.intersection(m_set2))
 # print(s_set)
-c = input()
-set_1 = set()
-for i in range(6): # (b)
-    set_1.add(int(input("Введите число: ")))
-print(set_1)
+# c = input()
+# set_1 = set()
+# for i in range(6): # (b)
+#     set_1.add(int(input("Введите число: ")))
+# print(set_1)
 
-b = input()
-set_2 = set()
-for i in range(6): # (b)
-    set_2.add(int(input("Введите число: ")))
-print(set_2)
+# b = input()
+# set_2 = set()
+# for i in range(6): # (b)
+#     set_2.add(int(input("Введите число: ")))
+# print(set_2)
 
-set_3 = sorted(set_1.intersection(set_2))
-print(set_3)
+# set_3 = sorted(set_1.intersection(set_2))
+# print(set_3)
 
 # Задача 28: Напишите рекурсивную функцию sum(a, b), 
 # возвращающую сумму двух целых неотрицательных чисел. 
@@ -338,3 +338,69 @@ print(set_3)
 #         list_2.append(i)
 # print(list_2)
 
+
+# черника
+# n = int(input("Введите кол-во кустов: "))
+# weight = [int(i) for i in input("Введите кол-во ягод на каждом кусте: ").split()][:n]
+# result = [weight[i - 1] + weight[i] + weight[(i + 1) % n] for i in range(len(weight))]
+# print(max(result))
+
+
+
+
+# Задача 34:  Винни-Пух попросил Вас посмотреть, 
+# есть ли в его стихах ритм. 
+# Поскольку разобраться в его кричалках не настолько просто, 
+# насколько легко он их придумывает, Вам стоит написать программу. 
+# Винни-Пух считает, что ритм есть, если число слогов (т.е. число гласных букв) 
+# в каждой фразе стихотворения одинаковое. Фраза может состоять из одного слова, 
+# если во фразе несколько слов, то они разделяются дефисами. 
+# Фразы отделяются друг от друга пробелами. 
+# Стихотворение  Винни-Пух вбивает в программу с клавиатуры.
+# В ответе напишите “Парам пам-пам”, если с ритмом все в порядке и “Пам парам”, 
+# если с ритмом все не в порядке
+
+# *Пример:*
+
+# **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
+#     **Вывод:** Парам пам-пам  
+# def vinni(str):
+#     str = str.split()
+#     list_1 = []
+#     for word in str:
+#         result = 0
+#         for i in word:
+#             if i in 'аеёиоуыэюя':
+#                 result += 1
+#         list_1.append(result)
+#     return len(list_1) == list_1.count(list_1[0])
+
+# print('Введите: пара-ра-рам рам-пам-папам па-ра-па-дам')
+
+# str = input()
+# if vinni(str):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, 
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы 
+# (подумайте, почему не с нуля). 
+# Примечание: бинарной операцией называется любая операция, 
+# у которой ровно два аргумента, как, например, у операции умножения.
+
+# *Пример:*
+
+# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+# **Вывод:**
+
+
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+#     for i in a:
+#         print(*[f"{x:>3}" for x in i])
+
+# print_operation_table(lambda x, y: x * y)
